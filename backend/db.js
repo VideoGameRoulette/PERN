d583ce634c78869af35b4644c3faba9c6c457946
Christopher D Couture <videogameroulette@yahoo.ca>
2022-03-06 05:51:09 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "551666545b837d480aed4c554cf4f13b",
+    password: "123456",
     host: "db",
     port: 5432,
     database: "userdb"
