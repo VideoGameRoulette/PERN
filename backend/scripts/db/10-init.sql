bf5c71bc0c107bdbe0a78f3a1ee40e330181f65c
unknown <videogameroulette@yahoo.ca>
2022-03-06 20:54:15 -0500

    more images for readme's and sql update

--- backend/scripts/db/10-init.sql
+++ backend/scripts/db/10-init.sql
@@ -1,5 +1,9 @@
+CREATE DATABASE userdb OWNER postgres;
+\c userdb;
 CREATE TABLE IF NOT EXISTS users(
     u_id UUID PRIMARY KEY,
     username VARCHAR(24),
     retro JSON
+    created_at TIMESTAMP WITH TIME ZONE,
+    updated_at TIMESTAMP WITH TIME ZONE,
 );
